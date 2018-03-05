c872de63c88f8a49031fed21c8d44e66e7927fda
Tarek Belkahia <tarek.belkahia@fabernovel.com>
2018-03-05 19:41:08 +0100

    Add iOS UI

--- ios/KotlinMultiplatformPlayground/ViewController.swift
+++ ios/KotlinMultiplatformPlayground/ViewController.swift
@@ -11,19 +11,48 @@ import KotlinMultiplatformPlaygroundLib
 
 class ViewController: UIViewController {
     
-    @IBOutlet var textView: UITextView?
+    @IBOutlet var lines: UIStackView?
+    @IBOutlet var state: UILabel?
+
+    let game = KMPLGame()
+    let size = KMPLGameCompanion().size
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let common = KMPLCommon()
-        textView?.text = "\(common.common()) \(common.platform())"
+        state?.text = "Next player: \(game.nextPlayer)"
+        (0..<size).forEach { x in
+            let line = UIStackView()
+            line.spacing = 10
+            line.distribution = .fillEqually
+            line.axis = .horizontal
+            (0..<size).forEach { y in
+                let cell = UIButton()
+                cell.backgroundColor = UIColor.lightGray
+                cell.tag = Int(x)
+                cell.addTarget(self, action: #selector(makeMove(_:)), for: .touchUpInside)
+                line.addArrangedSubview(cell)
+            }
+            lines?.addArrangedSubview(line)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func makeMove(_ sender: UIButton) {
+        let x = sender.tag
+        let y = lines?.subviews[x].subviews.index(of: sender)
+        let player = game.nextPlayer
+        if !game.isFinished() {
+            game.move(position: KMPLStdlibPair(first: x, second: y))
+            sender.setTitle(String(describing: player), for: .disabled)
+            sender.isEnabled = false
+            state?.text = "Next player: \(game.nextPlayer)"
+        }
+        if game.isFinished() {
+            if game.isDraw() {
+                state?.text = "Draw!"
+            } else {
+                state?.text = "Player \(String(describing: game.winner!)) won!"
+            }
+        }
     }
-
-
 }
 
